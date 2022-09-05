a89945ea7133fff65a7cb989d835d59a92cf09d6
SSAFY <kdw508@naver.com>
2022-09-05 16:15:41 +0900

    add PJT

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
+};
+
+export default App;
