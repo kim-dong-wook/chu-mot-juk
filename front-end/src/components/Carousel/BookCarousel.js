dc5d5ff514aa4f970e28af7cf546aaca1260ff88
전민재 <minjae77777@naver.com>
2022-09-07 10:25:51 +0900

    add main

--- front-end/src/components/Carousel/BookCarousel.js
+++ front-end/src/components/Carousel/BookCarousel.js
@@ -9,7 +9,7 @@ import './pagination.css';
 import 'swiper/css/navigation';
 const BookCarousel = () => {
   return (
-    <div className="p-20">
+    <div className="">
       <h1>제목</h1>
       <Swiper
         spaceBetween={10}
@@ -23,40 +23,40 @@ const BookCarousel = () => {
         loop={true}
         className="px-2"
       >
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 origin-left hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
           Slide 1
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 2
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 3
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700  hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 4
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 5
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 origin-right hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
           Slide 6
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 origin-left hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
           Slide 7
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 8
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 9
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 10
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
           Slide 11
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-150 duration-300 origin-right hover:z-50">
+        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
           Slide 12
         </SwiperSlide>
       </Swiper>
