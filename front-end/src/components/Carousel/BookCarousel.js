397961c7715f2dc14dffff0c493ae699050fe851
전민재 <minjae77777@naver.com>
2022-09-07 11:12:46 +0900

    add main2

--- front-end/src/components/Carousel/BookCarousel.js
+++ front-end/src/components/Carousel/BookCarousel.js
@@ -13,8 +13,8 @@ const BookCarousel = () => {
       <h1>제목</h1>
       <Swiper
         spaceBetween={10}
-        slidesPerView={6}
-        slidesPerGroup={6}
+        slidesPerView={7}
+        slidesPerGroup={7}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         navigation={true}
@@ -23,41 +23,47 @@ const BookCarousel = () => {
         loop={true}
         className="px-2"
       >
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
-          Slide 1
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
+          <img src="https://img.ridicdn.net/cover/4621001638/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 2
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/1377099102/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 3
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/3092017733/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 4
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/1811203363/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 5
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/2259009524/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
-          Slide 6
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://via.placeholder.com/485x670"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
-          Slide 7
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
+          <img src="https://via.placeholder.com/480x670"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 8
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-left hover:z-50">
+          <img src="https://img.ridicdn.net/cover/425306513/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 9
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/2008033538/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 10
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/2065072847/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
-          Slide 11
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/3076030032/xxlarge#1"></img>
         </SwiperSlide>
-        <SwiperSlide className="border-2 border-black w-60 h-80 items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
-          Slide 12
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/2065072847/xxlarge#1"></img>
+        </SwiperSlide>
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 hover:z-50">
+          <img src="https://img.ridicdn.net/cover/3076030032/xxlarge#1"></img>
+        </SwiperSlide>
+        <SwiperSlide className="w-60 h-[20.5rem] items-center flex justify-center hover:bg-sky-700 hover:scale-125 duration-300 origin-right hover:z-50">
+          <img src="https://img.ridicdn.net/cover/425297966/xxlarge#1"></img>
         </SwiperSlide>
       </Swiper>
     </div>
