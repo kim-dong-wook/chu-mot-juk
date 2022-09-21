fc7fb4a9dd060252ed653672ee605d401073d1aa
parkyein <iasfk@naver.com>
2022-09-21 15:36:40 +0900

    'add wc DB : 이상형월드컵 entity 추가'

---
+++ backend/src/main/java/com/ssafy/db/entity/WorldCup.java
@@ -0,0 +1,30 @@
+package com.ssafy.db.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class WorldCup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long wcNo;
+
+    @Column(nullable = false)
+    String novelName;
+
+    @Column(nullable = false)
+    String chName;
+
+    @Column(nullable = false, length = 6000)
+    String novelUrl;
+
+    @Column(nullable = false, length = 6000)
+    String pic;
+}
