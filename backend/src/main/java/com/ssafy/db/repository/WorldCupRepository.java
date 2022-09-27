aa8b6c3273ee6fa806560177a806571ef73b59fc
parkyein <iasfk@naver.com>
2022-09-27 16:00:34 +0900

    'fix query'

--- backend/src/main/java/com/ssafy/db/repository/WorldCupRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/WorldCupRepository.java
@@ -1,8 +1,8 @@
 package com.ssafy.db.repository;
 
 import com.ssafy.db.entity.WorldCup;
-import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface WorldCupRepository extends JpaRepository<WorldCup, Long> {
+    @Query(value = "select u from WorldCup u")
     Optional<List<WorldCup>> getWorldCups();
+
     Optional<WorldCup> getWorldCupByWcNo(Long wcNo);
 }
