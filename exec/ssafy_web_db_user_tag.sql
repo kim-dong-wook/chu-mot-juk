e53e0fefb9b081fc0531e75fc08fd9d499049240
박예인 <iasfk@naver.com>
2022-10-07 12:05:54 +0900

    Upload New File

---
+++ exec/ssafy_web_db_user_tag.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: j7a207.p.ssafy.io    Database: ssafy_web_db
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_tag`
+--
+
+DROP TABLE IF EXISTS `user_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_tag` (
+  `user_tag_no` bigint NOT NULL AUTO_INCREMENT,
+  `tag_no` bigint DEFAULT NULL,
+  `user_no` bigint DEFAULT NULL,
+  PRIMARY KEY (`user_tag_no`),
+  KEY `FK6kt4xr4oo9uw3n47yxuxoidqy` (`tag_no`),
+  KEY `FKoic87i5t0dtnicd39jhd2mjms` (`user_no`),
+  CONSTRAINT `FK6kt4xr4oo9uw3n47yxuxoidqy` FOREIGN KEY (`tag_no`) REFERENCES `tag` (`tag_no`),
+  CONSTRAINT `FKoic87i5t0dtnicd39jhd2mjms` FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=243 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tag`
+--
+
+LOCK TABLES `user_tag` WRITE;
+/*!40000 ALTER TABLE `user_tag` DISABLE KEYS */;
+INSERT INTO `user_tag` VALUES (2,54,2506),(3,55,2506),(7,84,2506),(8,74,2506),(9,58,2506),(10,6,2506),(18,1,2508),(19,2,2508);
+/*!40000 ALTER TABLE `user_tag` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-07 12:01:26
